de8dabad9144ab8c3ab5c5eb8defd077be0d2479
wenmianshi <134840821+wenmianshi@users.noreply.github.com>
2023-12-14 22:53:35 +0100

    11111111

--- miniprogram/pages/news/news.js
+++ miniprogram/pages/news/news.js
@@ -1,5 +1,17 @@
 Page({
-  clickMe: function() {
-    this.setData({ msg: "Hello World" })
-  }
+  data: {
+    message: "一般纳税人",
+    message1: "一般纳税人",
+    message2: "税务代理记账188元每月",
+  },
+  smallscopeTax: function() {
+    this.setData({message: "小规模纳税人"});
+  },
+  gaibian: function() {
+    this.setData({message1: "小规模纳税人"});
+  },
+  change: function() {
+    this.setData({message2: "税务代理记账88元每月"});
+  },
+  
 })
