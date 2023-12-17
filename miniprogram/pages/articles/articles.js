1805ea39722fa0766583b2ed871f77b03f09aefe
wenmianshi <134840821+wenmianshi@users.noreply.github.com>
2023-12-18 00:28:57 +0100

    12.18

--- miniprogram/pages/articles/articles.js
+++ miniprogram/pages/articles/articles.js
@@ -11,6 +11,71 @@ Page({
   onLoad: function () {
 
   },
+  aufsatz1() {
+    wx.navigateTo({
+      url: '/pages/aufsatz1/aufsatz1',
+    })
+  },
+  aufsatz2() {
+    wx.navigateTo({
+      url: '/pages/aufsatz2/aufsatz2',
+    })
+  },
+  aufsatz3() {
+    wx.navigateTo({
+      url: '/pages/aufsatz3/aufsatz3',
+    })
+  },
+  aufsatz4() {
+    wx.navigateTo({
+      url: '/pages/aufsatz4/aufsatz4',
+    })
+  },
+  aufsatz5() {
+    wx.navigateTo({
+      url: '/pages/aufsatz5/aufsatz5',
+    })
+  },
+  aufsatz6() {
+    wx.navigateTo({
+      url: '/pages/aufsatz6/aufsatz6',
+    })
+  },
+  aufsatz7() {
+    wx.navigateTo({
+      url: '/pages/aufsatz7/aufsatz7',
+    })
+  },
+  aufsatz8() {
+    wx.navigateTo({
+      url: '/pages/aufsatz8/aufsatz8',
+    })
+  },
+  aufsatz9() {
+    wx.navigateTo({
+      url: '/pages/aufsatz9/aufsatz9',
+    })
+  },
+  aufsatz10() {
+    wx.navigateTo({
+      url: '/pages/aufsatz10/aufsatz10',
+    })
+  },
+  aufsatz11() {
+    wx.navigateTo({
+      url: '/pages/aufsatz11/aufsatz11',
+    })
+  },
+  aufsatz12() {
+    wx.navigateTo({
+      url: '/pages/aufsatz12/aufsatz12',
+    })
+  },
+  aufsatz13() {
+    wx.navigateTo({
+      url: '/pages/aufsatz13/aufsatz13',
+    })
+  },
   onShareAppMessage() {
     return {
       title: 'scroll-view',
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -9,9 +9,12 @@ Page({
     interval: 3000,
     circular: true,
 
-    messageyi: "事关养老！12月31日前办理，才能享受这项个税优惠→",
-    messageer: "事关钱袋子！2024年度个税专项附加扣除信息确认开始啦~",
-    messagesan: "惠企政策查询+申报，宁企通“一站”汇聚！",
+    messageyi: "事关养老！12月31日前办理，才能享受",
+    messageyiyi: "\n这项个税优惠→",
+    messageer: "事关钱袋子！2024年度个税专项附加扣除",
+    messageerer: "\n信息确认开始啦~",
+    messagesan: "惠企政策查询+申报，宁企通“一站”汇",
+    messagesansan: "\n聚！",
 
     linkyi:"http://s59avscau.hd-bkt.clouddn.com/one.gif",
     linker:"http://s59avscau.hd-bkt.clouddn.com/twoo.png",
@@ -68,57 +71,69 @@ Page({
   },
   teil1: function(){
     this.setData({
-      messageyi: "事关养老！12月31日前办理，才能享受这项个税优惠→",
+      messageyi: "事关养老！12月31日前办理，才能享受",
+      messageyiyi: "\n这项个税优惠→",
       linkyi:"http://s59avscau.hd-bkt.clouddn.com/one.gif"
     });
     this.setData({
-      messageer: "事关钱袋子！2024年度个税专项附加扣除信息确认开始啦~",
+      messageer: "事关钱袋子！2024年度个税专项附加扣除",
+      messageerer: "\n信息确认开始啦~",
       linker:"http://s59avscau.hd-bkt.clouddn.com/twoo.png"
     });
     this.setData({
-      messagesan: "惠企政策查询+申报，宁企通“一站”汇聚！",
+      messagesan: "惠企政策查询+申报，宁企通“一站”汇",
+      messagesansan: "\n聚！",
       linksan:"http://s59avscau.hd-bkt.clouddn.com/three.png"
     });
   },
   teil2: function(){
     this.setData({
-      messageyi: "税务局提醒：2023年12月31日前，这项费用必须完成补缴，否则加收滞纳金！",
+      messageyi: "税务局提醒：2023年12月31日前，这项费用",
+      messageyiyi: "\n必须完成补缴，否则加收滞纳金！",
       linkyi:"http://s59avscau.hd-bkt.clouddn.com/four.jpeg"
     });
     this.setData({
-      messageer: "2024年专项附加扣除开始确认！财务请马上转给全体员工！12月31日前必须完成！否则影响明年到手工资！",
+      messageer: "2024年专项附加扣除开始确认！财务请马上",
+      messageerer: "\n转给全体员工！12月31日前必须完成！",
       linker:"http://s59avscau.hd-bkt.clouddn.com/five.png"
     });
     this.setData({
       messagesan: "进销不匹配，严查！",
+      messagesansan: "\n ",
       linksan:"http://s59avscau.hd-bkt.clouddn.com/sixxx.png"
     });
   },
   teil3: function(){
     this.setData({
       messageyi: "科普：小规模纳税人和一般人纳税人",
+      messageyiyi: "\n",
       linkyi:"http://s59avscau.hd-bkt.clouddn.com/seven.png"
     });
     this.setData({
-      messageer: "个人营业执照网上申请，个体工商户营业执照需要什么材料，一个人可以申请几个个体户呢？​",
+      messageer: "个人营业执照网上申请，个体工商户营业执照​",
+      messageerer: "\n需要什么材料，一个人可以申请几个个体户呢？",
       linker:"http://s59avscau.hd-bkt.clouddn.com/four.jpeg"
     });
     this.setData({
       messagesan: "大学生创业税费优惠政策",
+      messagesansan: "\n",
       linksan:"http://s59avscau.hd-bkt.clouddn.com/nine.jpeg"
     });
   },
   teil4: function(){
     this.setData({
-      messageyi: "2024年1月1日起实施！《企业知识产权合规管理体系 要求》标准换版",
+      messageyi: "2024年1月1日起实施！《企业知识产权合规",
+      messageyiyi: "\n管理体系 要求》标准换版",
       linkyi:"http://s59avscau.hd-bkt.clouddn.com/eight.png"
     });
     this.setData({
-      messageer: "超出营业执照经营范围可以开票吗？国家税务总局正式回复！",
+      messageer: "超出营业执照经营范围可以开票吗？国家税务",
+      messageerer: "\n总局正式回复！",
       linker:"http://s59avscau.hd-bkt.clouddn.com/four.jpeg"
     });
     this.setData({
-      messagesan: "【涨知识】享受技术转让、技术开发免征增值税，这些要点请注意",
+      messagesan: "【涨知识】享受技术转让、技术开发免征增",
+      messagesansan: "\n值税，这些要点请注意",
       linksan:"http://s59avscau.hd-bkt.clouddn.com/jishu.png"
     });
   },
